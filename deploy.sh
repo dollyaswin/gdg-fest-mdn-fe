3a75d75952e8f70d34bda995a92381b44004292b
Dolly Aswin <dolly.aswin@gmail.com>
2022-11-20 12:01:36 +0700

    #2 🚀 Deployment run

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+export PATH="$PATH:$HOME/$NODE_PATH";
+
 echo "Build..."
 rm -fr dist
 npm run build
+if [ ! -d "dist" ]; then echo "Deploy failed!"; exit 127; fi;
 
 echo "Deploy..."
 rsync --exclude '.git' --delete -av dist/gdg-fest-mdn-fe/. $APP_PATH/.
